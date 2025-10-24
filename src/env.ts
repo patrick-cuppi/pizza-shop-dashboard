0453859fa1b70aea59089bc6925904cf8234d8b0
Patrick Cuppi <cuppi.patrick@gmail.com>
2025-10-23 21:25:32 -0300

    feat(auth): create route to send an auth-link

--- src/env.ts
+++ src/env.ts
@@ -2,6 +2,8 @@ import { z } from "zod";
 
 const envSchema = z.object({
   DATABASE_URL: z.url().startsWith("postgresql://"),
+  API_BASE_URL: z.url(),
+  AUTH_REDIRECT_URL: z.url(),
 });
 
 export const env = envSchema.parse(process.env);
--- src/http/server.ts
+++ src/http/server.ts
@@ -1,42 +1,10 @@
-import { Elysia, t } from "elysia";
-import { db } from "../db/connection";
-import { restaurants, users } from "../db/schema";
+import { Elysia } from "elysia";
+import { registerRestaurantRoute } from "./routes/register-restaurant-route";
+import { sendAuthLinkRoute } from "./routes/send-auth-link-route";
 
-const app = new Elysia().get("/", () => "Hello, Elysia!");
+const app = new Elysia().use(registerRestaurantRoute).use(sendAuthLinkRoute);
 
-app.post(
-  "/restaurants",
-  async ({ body, set }) => {
-    const { restaurantName, managerName, email, phone } = body;
-
-    const [manager] = await db
-      .insert(users)
-      .values({
-        name: managerName,
-        email,
-        phone,
-        role: "manager",
-      })
-      .returning({
-        id: users.id,
-      });
-
-    await db.insert(restaurants).values({
-      name: restaurantName,
-      managerId: manager?.id,
-    });
-
-    set.status = 204;
-  },
-  {
-    body: t.Object({
-      restaurantName: t.String(),
-      managerName: t.String(),
-      email: t.String({ format: "email" }),
-      phone: t.String(),
-    }),
-  }
-);
+app.get("/health", () => "OK");
 
 app.listen(3333, () => {
   console.log("Server is running on http://localhost:3333");
