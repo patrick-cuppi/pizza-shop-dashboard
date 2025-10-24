126d200217bcd414620f16e852117215e82bcbfc
Patrick Cuppi <cuppi.patrick@gmail.com>
2025-10-24 13:53:00 -0300

    chore: setup jwt

--- src/env.ts
+++ src/env.ts
@@ -4,6 +4,7 @@ const envSchema = z.object({
   DATABASE_URL: z.url().startsWith("postgresql://"),
   API_BASE_URL: z.url(),
   AUTH_REDIRECT_URL: z.url(),
+  JWT_SECRET: z.string().min(32),
 });
 
 export const env = envSchema.parse(process.env);
--- src/http/server.ts
+++ src/http/server.ts
@@ -1,8 +1,23 @@
-import { Elysia } from "elysia";
+import { Elysia, t } from "elysia";
 import { registerRestaurantRoute } from "./routes/register-restaurant-route";
 import { sendAuthLinkRoute } from "./routes/send-auth-link-route";
+import jwt from "@elysiajs/jwt";
+import { env } from "../env";
+import cookie from "@elysiajs/cookie";
 
-const app = new Elysia().use(registerRestaurantRoute).use(sendAuthLinkRoute);
+const app = new Elysia()
+  .use(
+    jwt({
+      secret: env.JWT_SECRET,
+      schema: t.Object({
+        sub: t.String(),
+        restaurantId: t.Optional(t.String()),
+      }),
+    })
+  )
+  .use(cookie())
+  .use(registerRestaurantRoute)
+  .use(sendAuthLinkRoute);
 
 app.get("/health", () => "OK");
 
