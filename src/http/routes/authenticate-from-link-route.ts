11646b56180eeb2567d19fab1b8cb9f4850fd20f
Patrick Cuppi <cuppi.patrick@gmail.com>
2025-10-24 16:30:58 -0300

    feat: create route to sign-out and remove cookie

--- src/http/routes/authenticate-from-link-route.ts
+++ src/http/routes/authenticate-from-link-route.ts
@@ -7,7 +7,7 @@ import { auth } from "../auth";
 
 export const authenticateFromLinkRoute = new Elysia().use(auth).get(
   "/auth-links/authenticate",
-  async ({ query, jwt, cookie: { auth }, redirect }) => {
+  async ({ query, signUser, redirect }) => {
     const { code, redirect: redirectUrl } = query;
 
     const authLinkFromCode = await db.query.authLinks.findFirst({
@@ -33,17 +33,10 @@ export const authenticateFromLinkRoute = new Elysia().use(auth).get(
       },
     });
 
-    const token = await jwt.sign({
+    await signUser({
       sub: authLinkFromCode.userId,
       restaurantId: managedRestaurant?.id,
     });
-
-    if (!auth) throw new Error("Missing auth cookie");
-
-    auth.value = token;
-    auth.httpOnly = true;
-    auth.maxAge = 60 * 60 * 24 * 7; // 7 days
-    auth.path = "/";
 
     await db.delete(authLinks).where(eq(authLinks.code, code));
 
--- src/http/server.ts
+++ src/http/server.ts
@@ -2,11 +2,13 @@ import { Elysia } from "elysia";
 import { authenticateFromLinkRoute } from "./routes/authenticate-from-link-route";
 import { registerRestaurantRoute } from "./routes/register-restaurant-route";
 import { sendAuthLinkRoute } from "./routes/send-auth-link-route";
+import { signOutRoute } from "./routes/sign-out-route";
 
 export const app = new Elysia()
   .use(registerRestaurantRoute)
   .use(sendAuthLinkRoute)
-  .use(authenticateFromLinkRoute);
+  .use(authenticateFromLinkRoute)
+  .use(signOutRoute);
 
 app.get("/health", () => "OK");
 
